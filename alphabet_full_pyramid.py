b39994aba4b42a27885256c42adf5d1443807cb7
Anand Varrier <anand.varrier98@gmail.com>
2024-07-18 23:51:16 +0530

    Alphabet full pyramid 01

---
+++ alphabet_full_pyramid.py
@@ -0,0 +1,10 @@
+alpha = 65
+n = 5
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(" ", end="")
+    for k in range(1, 2*i):
+        print(chr(alpha), end="")
+        alpha+=1
+    alpha= 65
+    print()
