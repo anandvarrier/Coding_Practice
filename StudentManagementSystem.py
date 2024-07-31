80770a152280d7faf68536e310ec01977b522b63
Anand Varrier <anand.varrier98@gmail.com>
2024-07-31 07:23:07 +0530

    Updated code with search functionality

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -4,11 +4,15 @@ Student Management System
 students = {'Student': []}
 
 # Add Student function
-
 def addStudent(rno, nm, m):
     student = {'RollNumber': rno,
                'Name': nm,
                'Marks': m}
     students['Student'].append(student)
     return students
+
+# Search Student
+def searchStudent(rno):
+    print(list(students.keys())[list(students.values()).index(rno)])
+
 
--- StudentManagementSystem_driver.py
+++ StudentManagementSystem_driver.py
@@ -1,17 +1,19 @@
 from StudentManagementSystem import addStudent
 
-n = int(input('Enter the number of students: '))
 opt = int(input('Enter option\n 1.Add Student\n 2.Search Student\n 3.Delete Student\n 4.Update Student\n You Choice: '))
 
 if opt == 1:
+    n = int(input('Enter the number of students: '))
     for _ in range(n):
         rollno = int(input('Enter the roll no of the student: '))
         name = input('Enter the name of the student: ')
         marks = int(input('Enter  one subject marks: '))
         add = addStudent(rollno, name, marks)
     print(add)
+    opt
 elif opt == 2:
-    search = searchStudent()
+    search_rno = int(input('Enter the roll number you want to search: '))
+    search = searchStudent(search_rno)
 elif opt == 3:
     delete = delStudent()
 elif opt == 4:
