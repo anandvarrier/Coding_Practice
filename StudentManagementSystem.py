764cc3e1daee8e9c73d38ef42132dc403f3d954c
Anand Varrier <46650077+anandvarrier@users.noreply.github.com>
2024-08-08 23:50:16 +0530

    StudentManagementSystem.py
    Updated and correct logic

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -5,15 +5,23 @@ students = {}
 
 # Add Student function
 def addStudent(rno, nm, m):
-    students = {'Rollno': rno,
-                'Details': {'Name': nm,
-                            'Marks': m}}
+    students[rno] = {'Name': nm, 'Marks': m}
     return students
 
 # Search Student function
 def searchStudent(rollno):
-    for i in range(len(students)):
-        if students[i]['RollNumber'] == rollno:
-            print()
+    return students.get(rollno, None)
 
+# Delete Student function
+def delStudent(rollno):
+    if rollno in students:
+        del students[rollno]
+        return f"Student with roll number {rollno} has been deleted"
+    return None
 
+# Update Student function
+def updateStudent(rno, nm, m):
+    if rno in students:
+        students[rno] = {'Name': nm, 'Marks': m}
+        return f"Student with roll number {rno} has been updated"
+    return None
