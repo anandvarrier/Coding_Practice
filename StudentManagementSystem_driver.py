1c1d01178ad67ec6a0befb29d55e70d6a30d79d2
Anand Varrier <46650077+anandvarrier@users.noreply.github.com>
2024-08-08 23:50:59 +0530

    StudentManagementSystem_driver.py
    Updated and correct logic

--- StudentManagementSystem_driver.py
+++ StudentManagementSystem_driver.py
@@ -1,5 +1,4 @@
-from StudentManagementSystem import addStudent, searchStudent
-#delStudent, updateStudent, exitStudent
+from StudentManagementSystem import addStudent, searchStudent, delStudent, updateStudent
 
 while True:
     opt = int(input('Enter option\n 1.Add Student\n 2.Search Student\n 3.Delete Student\n 4.Update Student\n 5.Exit\n Your Choice: '))
@@ -8,18 +7,34 @@ while True:
         for _ in range(n):
             rollno = int(input('Enter the roll no of the student: '))
             name = input('Enter the name of the student: ')
-            marks = int(input('Enter  one subject marks: '))
+            marks = int(input('Enter one subject marks: '))
             add_result = addStudent(rollno, name, marks)
         print(add_result)
-        opt
     elif opt == 2:
         search_rno = int(input('Enter the roll number you want to search: '))
-        search = searchStudent(search_rno)
+        result = searchStudent(search_rno)
+        if result:
+            print(f"Student found: {result}")
+        else:
+            print("Student not found")
     elif opt == 3:
-        delete = delStudent()
+        delete_rno = int(input('Enter the roll number you want to delete: '))
+        result = delStudent(delete_rno)
+        if result:
+            print(result)
+        else:
+            print("Student not found")
     elif opt == 4:
-        update == updateStudent()
+        rollno = int(input('Enter the roll number you want to update: '))
+        name = input('Enter the new name of the student: ')
+        marks = int(input('Enter the new marks: '))
+        result = updateStudent(rollno, name, marks)
+        if result:
+            print(result)
+        else:
+            print("Student not found")
     elif opt == 5:
-        exit_sys = exitStudent()
+        print('Successfully Exiting.')
+        break
     else:
         print('Please enter a correct option')
