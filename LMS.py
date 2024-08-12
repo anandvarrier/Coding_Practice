e2d5156e3af8f4577279761ce53aba0ec2a11396
Anand Varrier <anand.varrier98@gmail.com>
2024-08-12 23:43:45 +0530

    Library Management System Code idea01

--- LMS.py
+++ LMS.py
@@ -6,4 +6,49 @@ There are 2 entities in this project
 Librarian who maintains the Book Inventory
 User who issues the books and returns
 
+Main Menu Options:
+Are you are a
+    A] Existing User
+    B] Existing Librarian
+    C] New User
+    D] New Librarian
+If Existing User
+    Ask for Authentication:
+        Check authentication conditions:
+            If user already exists, then give-> User already exist
+        If authentication fails
+            a) Failed to authenticate and Go back to Main Menu
+        If authentication is a success:            
+            User Menu:
+                1) Issue a book
+                2) Return a book
+                3) Have any other problem?
+                4) Give a feedback or suggestion
+                5) Go back to main menu
+If Existing Librarian:
+    Ask for Authentication:
+    Librarian Options:
+        1) View Inventory
+        2) Add Inventory
+        3) Delete Inventory
+        4) Update Inventory
+        5) View Feedback or suggestions
+        6) Go back to Main Menu
+
+If New User:
+    1) Enter First Name:
+    2) Enter Last Name
+    ) Enter Password (authentication)
+    4) Enter emailid (authentication)
+
+If New Librarian:
+    1) Enter First Name:
+    2) Enter Last Name
+    3) Enter Password (authentication)
+    4) Enter emailid (authentication)
+
 """
+
+# Main Menu
+
+
