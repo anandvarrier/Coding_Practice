8217616dbe79aef7dcbe8cfbb0dcab407e5794d9
Anand Varrier <46650077+anandvarrier@users.noreply.github.com>
2024-10-29 13:09:08 +0530

    Add files via upload

---
+++ Python_basics_evenonlytill20.py
@@ -0,0 +1,5 @@
+# Write a Python program to print all even numbers from 1 to 20.
+n = 20
+for i in range(1,21):
+    if i%2 == 0:
+        print(i)
---
+++ Python_basics_sumofnums.py
@@ -0,0 +1,7 @@
+# Calculate the sum of all numbers from 1 to a given number.
+
+n = int(input('Enter a number: '))
+summ = 0
+for i in range(1,n+1):
+    summ=summ+i
+print(summ)
