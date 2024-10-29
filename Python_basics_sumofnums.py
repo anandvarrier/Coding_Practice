f57c087d1531e2b15920a9625a2a1145066e43cd
Anand Varrier <46650077+anandvarrier@users.noreply.github.com>
2024-10-29 13:14:48 +0530

    Update Python_basics_sumofnums.py

--- Python_basics_sumofnums.py
+++ Python_basics_sumofnums.py
@@ -1,7 +1,14 @@
 # Calculate the sum of all numbers from 1 to a given number.
 
+# Approach 1
 n = int(input('Enter a number: '))
 summ = 0
 for i in range(1,n+1):
     summ=summ+i
 print(summ)
+
+print('*******')
+
+#Approach 2
+sum_result = sum(range(1, n + 1))
+print("Sum:", sum_result)
